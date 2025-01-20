5cc5c9222b9276abed2eca120118e905348b28a6
nawalkant145 <nawal2310145@akgec.ac.in>
2025-01-20 12:06:13 +0530

    final again again commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: 'CodeStream_Live_Project', // Set the repository name
-});
-
-
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // // https://vite.dev/config/
 // export default defineConfig({
-//   // Remove base path unless you're using a subdirectory
-//   // base: '/CodeStream_Live_Project/', // Comment this out for now
-//   plugins: [react()],
+//   base: 'CodeStream_Live_Project', // Set the repository name
 // });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  // Remove base path unless you're using a subdirectory
+  // base: '/CodeStream_Live_Project/', // Comment this out for now
+  plugins: [react()],
+});
 
